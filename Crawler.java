55219bc0c728282e83a9c16ef897768e16487169
adishjain <adish.jain1995@ymail.com>
2016-10-19 22:42:21 +0530

    Enhanced readability
    Comments added

--- Crawler.java
+++ Crawler.java
@@ -14,7 +14,7 @@ import org.jsoup.select.Elements;
 
 public class Crawler {
     public static void main(String[] args) throws IOException {
-
+        //Custom url goes here
         processPage("https://www.google.co.in/search?q=MSFT&hl=en&gl=in&as_drrb=b&authuser=0&source=lnt&tbs=cdr%3A1%2Ccd_min%3A10%2F10%2F2011%2Ccd_max%3A10%2F10%2F2011&tbm=nws");
     }
     public static void processPage(String URL) throws IOException {
@@ -22,10 +22,13 @@ public class Crawler {
             System.out.println("------ :\t\t" + URL+"\t\t: ------");
             Document doc = null;
             try {
+                //Just an agent definition. Don't worry about it
                 doc = Jsoup.connect(URL).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) " +
                         "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36").get();
+                //Class of <a> tag under which results are placed
                 Elements Tags = doc.select( "a.l._HId" );
                 Iterator<Element> tagIterator = Tags.iterator();
+                //link[] will contain the links to the result-webpages
                 String[] link = new String[10];
                 int i=0;
                 while (tagIterator.hasNext() && i<10){
@@ -51,6 +54,7 @@ public class Crawler {
         Document doc = null;
         doc = Jsoup.connect(linkI).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) " +
                 "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36").get();
+        //Selected the <p> tag here
         Elements Tags = doc.select( "p");
         System.out.println(Tags.outerHtml());
         System.out.println("-----------------------------------------------------");
