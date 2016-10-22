d350ccc63f9c43faa7eb601b253184c92a5cb4a6
LastStar <adish.jain1995@ymail.com>
2016-10-22 19:22:14 +0530

    Final Commit (#15)
    Thank You all for your precious contributions.

---
+++ src/main/java/fileHandling/SaveInFile.java
@@ -0,0 +1,65 @@
+package fileHandling;
+
+import POJO.ScrapedResult;
+import jdk.nashorn.api.scripting.JSObject;
+import org.json.simple.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Date;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.io.*;
+
+public class SaveInFile{
+	public void writeFile(ArrayList<ScrapedResult> list){
+
+        File file=new File("/scrapper.txt");
+        StringBuffer content=new StringBuffer();
+        JSONObject jsonObject = new JSONObject();
+        for (ScrapedResult scr:
+             list) {
+            jsonObject.put(scr.getDate(),scr.getText());
+            content.append(jsonObject);
+        }
+        BufferedWriter bw=null;
+        FileWriter fw=null;
+        try{
+            fw=new FileWriter(file.getName(),true);
+            bw=new BufferedWriter(fw);
+            bw.write("\n"+content.toString());
+            bw.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+//		for(ScrapedResult obj:list){
+//			 String d = obj.getDate();
+//			 ArrayList<String> text=obj.getText();
+//			 //ArrayList<String> text = new ArrayList<String>();
+//			 //text.add("HOLAAAA");
+//			 File file=new File("/scrapper.csv");
+//			 StringBuffer content=new StringBuffer();
+//			 //Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//			 content.append(d);
+//			 for(Object str:text){
+//				content.append(","+str.toString());
+//			}
+//			 BufferedWriter bw=null;
+//			 FileWriter fw=null;
+//			 try{
+//				fw=new FileWriter(file.getName(),true);
+//				bw=new BufferedWriter(fw);
+//				bw.write("\n"+content.toString());
+//				bw.close();
+//				}
+//			catch(IOException e){
+//				e.printStackTrace();
+//			}
+//		}
+		 
+	}
+}
